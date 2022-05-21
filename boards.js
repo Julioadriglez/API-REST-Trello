1f803ee44730a403494216dde959802f4b7b38eb
JulioAdriGlez <99068430+Julioadriglez@users.noreply.github.com>
2022-05-20 19:53:34 -0500

    Se intalo modulo de npm install node-fetch@2 por que no reconocia el modulo

--- boards.js
+++ boards.js
@@ -2,7 +2,7 @@
 // https://www.npmjs.com/package/node-fetch
 const fetch = require('node-fetch');
 
-fetch('https://api.trello.com/1/boards/{id}/memberships?key=APIKey&token=APIToken', {
+fetch('https://api.trello.com/1/members/me/boards?key=119ab0c01d35f77e9e73e11531be9cea&token=4252131b350daa10b8cfdb6cba6c1e234ad5e11b880a1136532bd635f3647ce2', {
   method: 'GET',
   headers: {
     'Accept': 'application/json'
